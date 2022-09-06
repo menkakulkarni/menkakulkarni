273a3fb713bdac068ecf0edb2cd2bc8cd707996c
Menka Kulkarni <112068972+menkakulkarni@users.noreply.github.com>
2022-09-06 16:37:33 +0530

    Add files via upload

---
+++ student.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+
+
+# In[2]:
+
+
+os.getcwd
+
+
+# In[3]:
+
+
+os.getcwd()
+
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[6]:
+
+
+pd.read_csv('STUDENT.TSV')
+
+
+# In[7]:
+
+
+import panda as pd
+
+
+# In[8]:
+
+
+import pandas as pd
+
+
+# In[10]:
+
+
+pd.read_csv("student21.tsv",sep="\t")
+
+
+# In[ ]:
+
+
+
+
